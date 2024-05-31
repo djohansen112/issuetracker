093a8dbb25abf635a1854dd0c05d9964efd14949
Dan Johansen <djohansen112@gmail.com>
2024-05-31 20:02:49 +1000

    connected to Prisma/Mysql

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Link from "next/link";
 import { AiFillBug } from "react-icons/ai";
 import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -21,7 +22,11 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
             href={link.href}
           >
             {link.label}
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const IssuesPage = () => {
-  return <div>ISSSHHOOOOOSS</div>;
+  return (
+    <div>
+      IssuesPage, Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+      fugiat dolorum ex accusamus dolorem reiciendis ullam deleniti a. Harum
+      illo praesentium, eveniet doloremque eligendi facere animi reprehenderit
+      ab veniam non?
+    </div>
+  );
 };
 
 export default IssuesPage;
